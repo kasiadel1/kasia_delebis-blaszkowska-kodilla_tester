097a79fb65c464cea8ef086381bac3a2f53510d3
Kasia Delebis <kasiadel@yahoo.com>
2021-08-24 14:54:06 +0200

    My First commit

---
+++ kodilla-intro/src/main/java/HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("Kasia");
+        System.out.println("41");
+        System.out.println("D");
+
+    }
+}
