e263d7bfff513d2ea870522242679b4e421184c2
Kasia Delebis <kasiadel@yahoo.com>
2021-09-15 23:28:28 +0200

    2.7 home work

---
+++ kodilla-intro/src/main/java/Book.java
@@ -0,0 +1,20 @@
+public class Book {
+
+        private String author;
+        private String title;
+
+        //dodawanie konstuktora z dwoma argumentami
+    public Book(String author, String title) {
+            this.author = author;
+            this.title = title;
+        }
+
+        //tworzenie metody of, przyjmujacej stringi i zwracajacej Book
+        public static Book of (String author, String title){
+            //tworzenie obiektu Book wewnatrz metody of
+            Book book = new Book(author, title);
+            return book;
+        }
+
+
+    }
