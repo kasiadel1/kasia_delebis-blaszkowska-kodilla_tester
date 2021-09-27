440178026d2fae223b83e82a08881a29493e1528
Kasia Delebis <kasiadel@yahoo.com>
2021-09-27 15:00:01 +0200

    3.6 tests name udpated

--- kodilla-basic-tests/src/test/java/com/kodilla/basic_assertion/CalculatorTestSuite.java
+++ kodilla-basic-tests/src/test/java/com/kodilla/basic_assertion/CalculatorTestSuite.java
@@ -24,31 +24,31 @@ public class CalculatorTestSuite {
             assertEquals(-3, substractResult);
         }
 // przyd 1 d> 0
-       /*     @Test
+           @Test
             public void testSquare1 () {
                 Calculator calculator = new Calculator();
                 double d = 5;
                 double square1Result = calculator.squared1(d);
                 assertEquals(25, square1Result, 0.02);
 
-            }*/
+            }
 
 // przyp 2 d<0
 
-    /*@Test
-    public void testSquare1 () {
+    @Test
+    public void testSquare2 () {
         Calculator calculator = new Calculator();
         double d = -5;
         double square1Result = calculator.squared1(d);
         assertEquals(25, square1Result, 0.02);
 
-    }*/
+    }
 
 
     //przyp 3 d=0
 
     @Test
-    public void testSquare1 () {
+    public void testSquare3 () {
         Calculator calculator = new Calculator();
         double d = 0;
         double square1Result = calculator.squared1(d);
