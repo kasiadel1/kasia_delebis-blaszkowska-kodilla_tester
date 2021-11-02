4949be6c86da9978fae9f61246ef67a35d2a0176
Kasia Delebis <kasiadel@yahoo.com>
2021-11-02 18:17:31 +0100

    6.5 homework update

--- kodilla-stream/src/test/java/com/kodilla/stream/homework/WarehouseTest.java
+++ kodilla-stream/src/test/java/com/kodilla/stream/homework/WarehouseTest.java
@@ -1,21 +1,32 @@
 package com.kodilla.stream.homework;
 
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 
 public class WarehouseTest {
 
+/*
    private Warehouse warehouse = new Warehouse();
 
    @Test
    public void checkIfOrderExists() throws OrderDoesntExistException {
       warehouse.addOrder(new Order("7"));
       Order result = warehouse.getOrder("7");
-      assertEquals("7", result.getNumber());
    }
+*/
+ private Warehouse warehouse = new Warehouse();
 
-
+   @Test
+   public void testIfOrderExists_withException(){
+      warehouse.addOrder(new Order("9"));
+      assertThrows(OrderDoesntExistException.class, () -> Warehouse.getOrder("99"));
+   }
 }
+
+
+
