c0ba9e6a7432cea126bc2eb531fe581c0390b741
Kasia Delebis <kasiadel@yahoo.com>
2021-11-30 18:04:21 +0100

    13.6 homework

---
+++ kodilla-google2-selenium/src/main/java/pages/GoogleSearch.java
@@ -0,0 +1,42 @@
+package pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.List;
+
+public class GoogleSearch extends AbstractPage {
+
+    @FindBy(css = "input[title='Szukaj']")
+    static WebElement inputField;
+
+    @FindBy(css = "input[value='GoogleSearch'']")
+    static List<WebElement> searchButton;
+    private GoogleResults googleResults;
+
+
+    public GoogleSearch(WebDriver driver) {
+        super(driver);
+    }
+        public void searchResults () {
+            PageFactory.initElements(driver, GoogleSearch.class);
+            inputField.sendKeys("Kodilla");
+            WebDriver driver = new ChromeDriver();
+            WebDriverWait wait = new WebDriverWait(driver, 10);
+            googleResults = loadResults(driver);
+            googleResults.iSeeResults();
+        }
+
+        public GoogleResults loadResults (WebDriver driver){
+            WebDriverWait wait = new WebDriverWait(driver, 40);
+            wait.until(ExpectedConditions.elementToBeClickable(searchButton.get(0))).click();
+            GoogleResults googleResults = new GoogleResults(driver);
+            return googleResults;
+        }
+    }
